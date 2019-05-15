309939c94f0a21323586f2c820f381edb0dcf344
SarthakSri98 <sarthaksri1998@gmail.com>
2019-05-15 07:48:41 +0530

    deployed to heroku

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use((req,res,next)=>{
 app.use("/catalog", catalogRouter);
 app.use("/users",userRouter);
 
-// app.use((req,res,next)=>{
-//     res.sendFile(path.join(__dirname,"angular","index.html"))
-// })
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,"angular","index.html"))
+})
 module.exports = app;
